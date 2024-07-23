cfec13fb035b669954b5604a816a2c6557655b31
zaryab <zaryabdeveloper49@gmail.com>
2024-07-23 05:02:45 +0500

    new work added

--- src/app/app-contant.ts
+++ src/app/app-contant.ts
@@ -1,13 +1,3 @@
 export class AppConstants {
 
-static readonly TOASTR_CONFIG = {
-    timeOut: 3000,
-    extendedTimeOut: 1000,
-    disableTimeOut: false,
-    positionClass: 'toast-bottom-right',
-    closeButton: true,
-    tapToDismiss: true,
-    preventDuplicates: true,
-    progressBar: true
-  };
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { FeaturesComponent } from './features/features.component';
 import { ContactComponent } from './features/contact/contact.component';
 import { FeaturedProductsComponent } from './features/featured-products/featured-products.component';
+import { AboutComponent } from './features/about/about.component';
 
 export const routes: Routes = [
     { path: '', component: FeaturesComponent ,
         children: [
             { path: '', component: FeaturedProductsComponent },
+            { path: 'about', component: AboutComponent },
             { path: 'contact', component: ContactComponent },
           ]
     },
